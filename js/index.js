b9fc89aaf1ca0adc89b41d4cc2344d47812acdc0
Olena Kozakova <helen_kozakova@ukr.net>
2021-04-12 17:21:49 +0300

    fixid code

--- js/index.js
+++ js/index.js
@@ -15,39 +15,42 @@ class CountdownTimer {
 setInt = setInterval(() => {
     const newDate = Date.now();
     const timeClock = this.targetDate - newDate;
-    this.updateClockface(timeClock);
+    const time = this.updateClockface(timeClock);
+    this.updateClock(time);
     this.timeFinish(timeClock);
-  }, 1000);
+}, 1000);
+
+  updateClock({ days, hours, mins, secs }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+};
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
 
   updateClockface(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.mins.textContent = `${mins}`;
-    refs.secs.textContent = `${secs}`;
-    //return updateClock({ days, hours, mins, secs });
+
+     return { days, hours, mins, secs }
   }
 
-//   function updateClock({ days, hours, mins, secs }) {
-//     days.textContent = days;
-//     hours.textContent = hours;
-//     mins.textContent = mins;
-//     secs.textContent = secs;
-// };
-
-  pad(value) {
-    return String(value).padStart(2, "0");
-  }
   timeFinish(timeClock) {
     if (timeClock < 0) {
       clearInterval(this.setInt);
       refs.timerMessage.textContent = "Happy Merry Christmas!";
+      const eventTimer = document.querySelector('.event-description');
+         eventTimer.remove();
+     }
+     return
     }
-  }
-};
+  };
+
 new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Dectmber 25, 2021"),
