f9e23d38ed87f7e369ea2489ee8e5466a0161a0c
Olena Kozakova <helen_kozakova@ukr.net>
2021-04-12 17:45:03 +0300

    fixid code

--- js/index.js
+++ js/index.js
@@ -20,6 +20,15 @@ setInt = setInterval(() => {
     this.timeFinish(timeClock);
 }, 1000);
 
+  updateClockface(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+     return { days, hours, mins, secs }
+  }
+
   updateClock({ days, hours, mins, secs }) {
     refs.days.textContent = `${days}`;
     refs.hours.textContent = `${hours}`;
@@ -29,15 +38,6 @@ setInt = setInterval(() => {
 
   pad(value) {
     return String(value).padStart(2, "0");
-  }
-
-  updateClockface(time) {
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
-     return { days, hours, mins, secs }
   }
 
   timeFinish(timeClock) {
